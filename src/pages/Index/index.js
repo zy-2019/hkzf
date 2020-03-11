cc33afaa47962bb7ffe499e1bc4992cdbf9030f3
Your Name <you@example.com>
2020-03-11 11:26:14 +0800

    首页-轮播图-配置

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react'
 
+//轮播图
+import { Carousel } from 'antd-mobile';
 
 
 export class Index extends Component {
-    render() {
+
+    
+    state = {
+        data: ['1', '2', '3'],  //轮播图状态数据
+
+        //设置轮播图高度
+        imgHeight: 176,
+      }
+      componentDidMount() {
+        // simulate img loading
+        setTimeout(() => {
+          this.setState({
+            data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+          });
+        }, 100);
+      }
+      render() {
         return (
-            <div>
-                <h1>默认首页</h1>
-            </div>
-        )
-    }
+          <div>
+            <Carousel
+
+              autoplay={true}
+
+              infinite //无限  相当于无缝轮播
+            //   beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+            //   afterChange={index => console.log('slide to', index)}
+            >   
+
+            {/* 列表渲染 */}
+              {this.state.data.map(val => (
+                <a
+                  key={val}
+                  href="http://www.alipay.com"
+                  style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                >
+                  <img
+                    src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                    alt=""
+                    style={{ width: '100%', verticalAlign: 'top' }}
+                    onLoad={() => {
+                      // fire window resize event to change height
+                      window.dispatchEvent(new Event('resize'));
+                      this.setState({ imgHeight: 'auto' });
+                    }}
+                  />
+                </a>
+              ))}
+            </Carousel>
+          </div>
+        );
+      }
 }
 
 export default Index
