6eec489c2c389c4aafcfc1fd043c000c68d81b9d
Your Name <you@example.com>
2020-03-13 00:11:30 +0800

    城市选择-请求城市列表数据

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,7 +1,22 @@
 
 import React, { Component } from 'react'
 
+import { getCityList } from '../../utils/api/city'
+
 export default class CityList extends Component {
+
+
+    //生命周期  用来操作dom 发送ajax请求
+    componentDidMount(){
+        this.getCityList()
+    }
+    //获取城市列表数据
+
+    getCityList = async()=>{
+        const res = await getCityList()
+        console.log('城市列表数据',res);
+    }
+
     render() {
         return (
             <div>
--- src/utils/api/city/index.js
+++ src/utils/api/city/index.js
@@ -2,12 +2,23 @@
 
 import axios from  '../../axios'  //请求路劲不能错
 
-//轮播图请求
+//获取当前城市
 export function getCityInfo(name) {
     //返回一个promise对象
     return axios.get('/area/info',{
         params:{
             name
+        }
+    })
+}
+
+
+//获取城市列表数据
+export function getCityList(level=1) {
+    //返回一个promise对象
+    return axios.get('/area/city',{
+        params:{
+            level
         }
     })
 }
